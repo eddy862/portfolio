341da8e355e389d6d2560a8d077c69d1c1e63bcf
Eddy <limeddy1125@gmail.com>
2024-10-06 13:43:51 +0800

    finalise

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,22 @@ import {FloatingNav} from "@/components/ui/FloatingNavbar"
 import { navItems } from "@/data";
 import RecentProjects from "@/components/RecentProjects"
 import Client from "@/components/Client";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-5 sm:px-10">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto px-5 sm:px-10 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Client />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+import Image from "next/image";
+
+function Footer() {
+  return (
+    <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50"
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take <span className="text-purple">your</span> digital
+          presence to the next level?
+        </h1>
+
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          React out to me today and let's discuss how I can help you achieve
+          your goals.
+        </p>
+
+        <a href="limeddy1125@gmail.com">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center gap-y-4">
+        <p className="text-sm md:text-base md:font-normal font-light">
+          Copyright © Eddy
+        </p>
+
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-150 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+              <Image
+                src={profile.img}
+                alt={profile.img}
+                width={20}
+                height={20}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
